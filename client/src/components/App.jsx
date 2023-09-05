d05e49e3647c13efa1cb7b63126436c81dfda42d
Michael Del Pape <m4delpape@gmail.com>
2023-09-04 19:05:27 -0700

    Initialized site and added a rotating cube

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Scene from './Scene.jsx';
+
+export default function App() {
+  return (
+    <div id="App">
+      <div>Header</div>
+      <Scene />
+    </div>
+  );
+}
---
+++ client/src/components/Scene.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+const Scene = () => {
+  const canvasRef = useRef(null);
+  const animationFrameId = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: false });
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+
+    const camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000
+    );
+    camera.position.z = 5;
+
+    // Geometry and Material (Example: simple cube)
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const renderScene = () => {
+      renderer.render(scene, camera);
+    };
+
+    const animate = () => {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderScene();
+
+      animationFrameId.current = requestAnimationFrame(animate);
+    };
+
+    const handleResize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      renderScene();  // Re-render the scene
+    };
+
+    window.addEventListener('resize', handleResize);
+    animate();  // Start the animation loop
+
+    // Cleanup: remove event listener and cancel animation frame on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      if (animationFrameId.current) {
+        cancelAnimationFrame(animationFrameId.current);
+      }
+    };
+
+  }, []);
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <canvas ref={canvasRef} className="scene"></canvas>
+    </div>
+  );
+};
+
+export default Scene;
