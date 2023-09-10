0d53bbc618fb7666fae83449653cce7ba97e635c
Michael Del Pape <m4delpape@gmail.com>
2023-09-10 13:51:32 -0700

    Update index.js

--- docs/server/index.js
+++ docs/server/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../')));
 
 
 
